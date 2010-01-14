711a94e1ff1f4dd2724822e463a7f0475f484115
Fabian Linzberger <e@lefant.net>
2010-01-14 13:11:25 +0100

    licensing improvements

--- KurtLib.hs
+++ KurtLib.hs
@@ -1,5 +1,33 @@
 {-# OPTIONS -O2 -Wall -Werror -Wwarn #-}
 
+{-
+Copyright (C) 2010 Fabian Linzberger <e@lefant.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+-}
+
+{- |
+   Module     : KurtLib
+   Copyright  : Copyright (C) 2010 Fabian Linzberger
+   License    : GNU GPL, version 3 or above
+
+   Maintainer : Fabian Linzberger <e@lefant.net>
+   Stability  : experimental
+   Portability: probably
+
+Nothing here yet, move on.
+
+Written by Fabian Linzberger, e\@lefant.net
+-}
+
 module KurtLib (
 
               ) where
