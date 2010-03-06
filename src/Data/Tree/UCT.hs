7ebfd62ae477fff2890305ab35130590721071ad
Fabian Linzberger <e@lefant.net>
2010-03-06 18:31:58 +0100

    the big uct refactoring, part one

--- src/Data/Tree/UCT.hs
+++ src/Data/Tree/UCT.hs
@@ -13,199 +13,131 @@ UCT tree search using Data.Tree.Zipper for updates in the Tree
 
 -}
 
-module Data.Tree.UCT ( uct
-                     , uctZipperDown
-                     , makeNodeWithChildren
-                     , principalVariation
+module Data.Tree.UCT ( rootNode
+                     , selectNode
+                     , policyUCB1
+                     , expandNode
+                     , backpropagate
                      ) where
 
 
-import Control.Monad.Random (Rand, RandomGen, evalRand)
 import Data.Maybe (fromJust)
 import Data.List (unfoldr, maximumBy)
 import Data.Ord (comparing)
+import Data.Word (Word)
 import Data.Tree (Tree(..))
-import Data.Tree.Zipper (TreeLoc, tree, fromTree, hasChildren, setTree, parent, getChild)
+import Data.Tree.Zipper (TreeLoc, tree, fromTree, isRoot, hasChildren, parent, findChild, modifyTree, modifyLabel)
 -- import Debug.Trace (trace)
 
 import Data.Tree.UCT.GameTree
 
 
-exploratoryC :: Float
-exploratoryC = 1
-
-uct :: (UctNode a, RandomGen g) => a -> g -> Tree (UctLabel a)
-uct initState rGen =
-    tree $ evalRand (uctZipper (fromTree $ makeNodeWithChildren initState)) rGen
+exploratoryC :: Double
+exploratoryC = 0.4
 
 
-uctZipper :: (UctNode a, RandomGen g) =>
-             TreeLoc (UctLabel a) ->
-             Rand g (TreeLoc (UctLabel a))
-uctZipper loc = do
-  (loc', done) <- uctZipperDown loc
-  case done of
-    True -> return loc'
-    False -> uctZipper loc'
+
+rootNode :: (UCTNode a) => [a] -> UCTTreeLoc a
+rootNode moves =
+    fromTree $ Node (nodeFromMove undefined) $ subForestFromMoves moves
 
 
-uctZipperDown  :: (UctNode a, RandomGen g) => TreeLoc (UctLabel a) -> Rand g ((TreeLoc (UctLabel a)), Bool)
-uctZipperDown loc =
-    if hasChildren loc
-    then
-        uctZipperDown $ chooseUctMax loc
-    else
-        if isTerminalNode state
-        then uctZipperUp loc (finalResult state) True
-        else (do
-               result <- randomEvalOnce state
-               uctZipperUp loc' result False)
+-- selection section
+-----------------------------------
+
+type UCTPolicy a = (UCTTree a -> UCTTree a)
+-- selects a leaf according to in-tree selection policy
+-- default UCB1, should be pluggable
+selectNode :: (UCTNode a) => UCTPolicy a -> UCTTreeLoc a
+           -> (UCTTreeLoc a, [a])
+selectNode policy loc =
+    (leaf, leafPath leaf)
     where
-      state = nodeState $ rootLabel node
-      node = tree loc
-
-      loc' = setTree node' loc
-      node' = node { subForest = makeSubForest state }
-
-
-
-uctZipperUp  :: (UctNode a, RandomGen g) => TreeLoc (UctLabel a) -> Float -> Bool -> Rand g ((TreeLoc (UctLabel a)), Bool)
-uctZipperUp loc result done =
-    case parent loc' of
-      Nothing ->
-          return (loc', done)
-      Just parentLoc ->
-          -- if evaluating this subtree is finished
-          if done
-          then
-              -- one perfect move is enough to make parent
-              -- a losing move
-              if result > 0.8
-              then
-                  -- trace ("uctZipperUp: result 1, done "
-                  --            ++ show ("parent", rootLabel parentNode)
-                  --            ++ show ("label'", label')
-                  --           )
-                  uctZipperUp parentLoc 0 True
-              else
-                  -- if all siblings are also done, then parent
-                  -- is also done with 1 - (max of sibling scores)
-                  if all (isDone . rootLabel) $ subForest parentNode
-                  then
-                      -- trace ("uctZipperUp: all done "
-                      --        ++ show ("parent", rootLabel parentNode)
-                      --        ++ show ("label'", label')
-                      --        ++ show ("result''", result'')
-                      --        ++ "\n\nsubforest\n"
-                      --        ++ (show $ map rootLabel $ subForest parentNode)
-                      --       )
-                      uctZipperUp parentLoc result'' True
-                  else
-                      -- trace ("uctZipperUp: done, but active siblings left " ++ show label' ++ (show $ map rootLabel $ filter (not . isDone . rootLabel) $ subForest parentNode))
-                      uctZipperUp parentLoc result' False
-          else
-              uctZipperUp parentLoc result' False
+      leaf = head $ snd $ span hasChildren $ iterate selectNode' loc
+      selectNode' loc' =
+          -- fromMaybe (error ("selectNode' findChild returned Nothing "
+          --                   ++ show (selectedTree, loc)))
+          fromJust $ findChild ((==) selectedTree) loc'
           where
-            parentNode = tree parentLoc
-            maxResult =
-                winningProb $ rootLabel $
-                            maximumBy
-                            (comparing (winningProb . rootLabel))
-                            $ subForest parentNode
-            result'' = 1 - maxResult
+            selectedTree = policy $ tree loc'
+
+
+policyUCB1 :: UCTNode a => UCTPolicy a
+policyUCB1 node =
+    maximumBy
+    (comparing ((ucb1 parentVisits) . rootLabel))
+    $ subForest node
     where
-      loc' = setTree node' loc
-      node' = node { rootLabel = label' }
-      label' = label { winningProb = newProb, visits = (oldCount + 1), isDone = done }
+      parentVisits = nodeVisits $ rootLabel node
 
-      newProb =
-          if done
-          then result
-          else updateProb oldProb oldCount result
-      oldProb = winningProb label
-      oldCount = visits label
-      label = rootLabel node
-      node = tree loc
-
-      result' = 1 - result
+ucb1 :: UCTNode a => Word -> MoveNode a -> Double
+ucb1 parentVisits node =
+    (nodeValue node)
+    + (exploratoryC
+       * (sqrt
+          ((log (fromIntegral parentVisits))
+           / (fromIntegral (nodeVisits node)))))
 
 
 
-makeNodeWithChildren :: (UctNode a) => a -> Tree (UctLabel a)
-makeNodeWithChildren state =
-    Node { rootLabel = defaultUctLabel { nodeState = state }
-         , subForest = makeSubForest state
+-- expansion
+----------------------------------
+
+expandNode :: UCTNode a => UCTTreeLoc a -> [a] -> UCTTreeLoc a
+expandNode loc children =
+    modifyTree expandChildren loc
+    where
+    expandChildren node =
+        node { subForest = subForestFromMoves children }
+
+subForestFromMoves :: UCTNode a => [a] -> UCTForest a
+subForestFromMoves moves =
+    map (((flip Node) []) . nodeFromMove) moves
+
+
+-- simulation needs to be handled by game code
+-----------------------------------
+
+
+
+
+
+
+-- backpropagation section
+-----------------------------------
+
+-- computes list of moves needed to reach the passed leaf loc from the root
+leafPath :: UCTNode a => UCTTreeLoc a -> [a]
+leafPath initLoc =
+    reverse $ unfoldr f initLoc
+    where
+      f loc =
+          case parent loc of
+            Just loc' ->
+                Just (nodeMove $ rootLabel $ tree loc, loc')
+            Nothing ->
+                Nothing
+
+
+-- updates node with a new value
+updateNodeValue :: UCTNode a => Double -> MoveNode a -> MoveNode a
+updateNodeValue value node =
+    node { nodeVisits = newVisits
+         , nodeValue = ((oldValue * (fromIntegral oldVisits)) + value)
+                       / fromIntegral newVisits
          }
-
-makeSubForest :: (UctNode a) => a -> [Tree (UctLabel a)]
-makeSubForest state =
-    map makeLeafNode $ children state
-
-makeLeafNode :: (UctNode a) => a -> Tree (UctLabel a)
-makeLeafNode state =
-    Node { rootLabel = defaultUctLabel { nodeState = state },
-           subForest = [] }
-
-
-
-chooseUctMax :: (UctNode a) => TreeLoc (UctLabel a) -> TreeLoc (UctLabel a)
-chooseUctMax loc =
-  -- trace ("chooseUctMax: "
-  --        ++ show ((rootLabel $ tree loc),
-  --                 fst uctMaxChildPair,
-  --                 (uctValue parentVisits $ fst uctMaxChildPair)))
-  uctMaxChild
-  where
-    uctMaxChild =
-        fromJust $ getChild (snd uctMaxChildPair) loc
-
-    uctMaxChildPair =
-        maximumBy
-        (comparing ((uctValue parentVisits) . fst))
-        activeSubtrees
-
-    parentVisits = visits $ rootLabel $ tree loc
-
-    activeSubtrees =
-        filter (not . isDone . fst) numberedForest
-    numberedForest = zip (map rootLabel $ subForest (tree loc)) [1..]
-
-uctValue :: (UctNode a) => Int -> (UctLabel a) -> Float
-uctValue parentVisits node =
-    -- trace ("uctValue: "
-    --        ++ show node
-    --        ++ show (parentVisits, winningProb node, value))
-    value
     where
-      value =
-          (winningProb node)
-          + (exploratoryC
-             * (sqrt
-                ((log (fromIntegral parentVisits))
-                 / (fromIntegral (visits node)))))
+      oldVisits = nodeVisits node
+      newVisits = succ oldVisits
+      oldValue = nodeValue node
 
 
-
-
-
-updateProb :: Float -> Int -> Float -> Float
-updateProb oldProb oldCount result =
-    ((oldProb * (fromIntegral oldCount)) + result) / (fromIntegral (oldCount + 1))
-
-
-
-principalVariation :: (UctNode a) => Tree (UctLabel a) -> [(UctLabel a)]
-principalVariation t =
-    unfoldr maxChild t
-
-maxChild :: Tree (UctLabel a) -> Maybe ((UctLabel a), Tree (UctLabel a))
-maxChild t =
-    case subForest t of
-      [] -> Nothing
-      forest ->
-          Just (rootLabel mNode, mNode)
-              where
-                mNode = maximumBy
-                        (comparing (visits . rootLabel))
-                        forest
+backpropagate :: UCTNode a => Double -> UCTTreeLoc a -> UCTTreeLoc a
+backpropagate value loc =
+    if isRoot loc
+    then loc'
+    else backpropagate value' loc'
+    where
+      loc' = modifyLabel (updateNodeValue value') loc
+      value' = updateBackpropagationValue (nodeMove $ rootLabel $ tree $ loc) value
 
--- src/Data/Tree/UCT/GameTree.hs
+++ src/Data/Tree/UCT/GameTree.hs
@@ -14,57 +14,110 @@ UCT tree search using Data.Tree.Zipper for updates in the Tree
 
 -}
 
-module Data.Tree.UCT.GameTree ( UctNode(..)
-                              , UctLabel(..)
-                              , defaultUctLabel
+module Data.Tree.UCT.GameTree ( UCTTreeLoc
+                              , UCTTree
+                              , UCTForest
+                              , MoveNode(..)
+                              , nodeFromMove
+                              , UCTNode(..)
+                              -- , UctNode(..)
+                              -- , UctLabel(..)
+                              -- , defaultUctLabel
                               ) where
 
 
+import Data.Tree (Tree, Forest)
+import Data.Tree.Zipper (TreeLoc)
 
-import Control.Monad.Random (Rand, RandomGen)
-import Text.Printf (printf)
+import Data.Word (Word)
+
+-- import Control.Monad.Random (Rand, RandomGen)
+-- import Text.Printf (printf)
+
 -- import Debug.Trace (trace)
 
 
 
-class (Show a) => UctNode a where
-    isTerminalNode :: a -> Bool
-    finalResult :: a -> Float
-    randomEvalOnce :: (RandomGen g) => a -> Rand g Float
-    children :: a -> [a]
-
-instance (UctNode a) => Show (UctLabel a) where
-    show label =
-        show (nodeState label) ++ " " ++
-        printf "%.2f " (winningProb label) ++
-        show (visits label) ++ dStr ++ " - "
-        where
-          dStr = case isDone label of
-                   True -> "+ "
-                   False -> ""
-
-data UctLabel a = UctLabel {
-     nodeState       :: a
-    ,winningProb     :: Float
-    ,visits          :: Int
-    ,isDone          :: Bool
-    }
+type UCTTreeLoc a = TreeLoc (MoveNode a)
+type UCTTree a = Tree (MoveNode a)
+type UCTForest a = Forest (MoveNode a)
 
 
 
-instance Eq (UctLabel a) where
-    (==) a b =
-        (winningProb a == winningProb b)
-        && (visits a == visits b)
-        && (isDone a == isDone b)
+-- things the game logic must be able to provide for moves
+class (Eq a, Show a) => UCTNode a where
+    initialMoveValue :: a -> Double
+    updateBackpropagationValue :: a -> Double -> Double
 
 
-defaultUctLabel :: UctLabel a
-defaultUctLabel = UctLabel {
-                    nodeState = undefined
-                  , winningProb = 0.5
-                  -- , uctValue = 0.5
-                  , visits = 1
-                  , isDone = False
-                  }
+data MoveNode a = MoveNode { nodeMove   :: a
+                           , nodeVisits :: !Word
+                           , nodeValue  :: Double
+                           }
+                  deriving (Show)
+
+instance (UCTNode a) => Eq (MoveNode a) where
+    (==) a b = nodeMove a == nodeMove b
+
+-- instance (Show a) => Show (MoveNode a) where
+--     show node = show $ nodeMove node
+
+nodeFromMove :: (UCTNode a) => a -> MoveNode a
+nodeFromMove move = MoveNode { nodeMove = move
+                             , nodeVisits = 0
+                             , nodeValue = initialMoveValue move }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- class (Show a) => UctNode a where
+--     isTerminalNode :: a -> Bool
+--     finalResult :: a -> Float
+--     randomEvalOnce :: (RandomGen g) => a -> Rand g Float
+--     children :: a -> [a]
+
+-- instance (UctNode a) => Show (UctLabel a) where
+--     show label =
+--         show (nodeState label) ++ " " ++
+--         printf "%.2f " (winningProb label) ++
+--         show (visits label) ++ dStr ++ " - "
+--         where
+--           dStr = case isDone label of
+--                    True -> "+ "
+--                    False -> ""
+
+-- data UctLabel a = UctLabel {
+--      nodeState       :: a
+--     ,winningProb     :: Float
+--     ,visits          :: Int
+--     ,isDone          :: Bool
+--     }
+
+-- instance Eq (UctLabel a) where
+--     (==) a b =
+--         (winningProb a == winningProb b)
+--         && (visits a == visits b)
+--         && (isDone a == isDone b)
+
+
+-- defaultUctLabel :: UctLabel a
+-- defaultUctLabel = UctLabel {
+--                     nodeState = undefined
+--                   , winningProb = 0.5
+--                   -- , uctValue = 0.5
+--                   , visits = 1
+--                   , isDone = False
+--                   }
 
