3a04be01788ea59c97d3365ef23500ba490ab500
Fabian Linzberger <e@lefant.net>
2010-03-14 20:26:32 +0100

    towards incremental goban, part2: initial data types, helpers, addStone (adjacent same color chain handling)

--- src/Data/Goban/Incremental.hs
+++ src/Data/Goban/Incremental.hs
@@ -15,42 +15,51 @@ Incrementally updated stone chains including liberty count for goban.
 -}
 
 module Data.Goban.Incremental ( Chain(..)
+                              , newChainGoban
+                              , showChainIdGoban
+                              , deleteChain
+                              , mapDeleteChain
+                              , vertexChain
                               ) where
 
 
 
-import Control.Monad (liftM, filterM)
+-- import Control.Monad (liftM, filterM)
 import Control.Monad.ST (ST)
--- import Data.Maybe (catMaybes)
+import Data.Maybe (fromMaybe, catMaybes)
 -- import Data.Word (Word)
--- import Data.List ((\\), nub)
+import Data.List (foldl', sort, group, partition, (\\))
 import Text.Printf (printf)
 
 
 import qualified Data.IntMap as M
-import qualified Data.IntSet as S
+import qualified Data.Set as S
 import qualified Data.Array.ST as A
 
 
 
-import Debug.TraceOrId (trace)
+-- import Debug.TraceOrId (trace)
 
 
 import Data.Goban.Types
 import Data.Goban.IntVertex
 
 
-data Chain = Chain { chainId        :: !ChainId
-                   , chainColor     :: !Color
-                   , chainLiberties :: !LibertyCount
-                   , chainVertices  :: ![Vertex]
+data Chain = Chain { chainId              :: !ChainId
+                   , chainColor           :: !Color
+                   , chainLiberties       :: !VertexSet
+                   , chainVertices        :: !VertexSet
+                   , chainNeighbours      :: !ChainNeighbours
                    }
            deriving (Show)
 
 type ChainId = Int
+noChainId :: ChainId
+noChainId = 0
 
-type LibertyCount = Int
+-- type LibertyCount = Int
 
+type VertexSet = S.Set Vertex
 
 -- maybe also reconsider Vector for this?
 type ChainMap = M.IntMap Chain
@@ -58,15 +67,18 @@ type ChainMap = M.IntMap Chain
 -- try this instead of Vector this time
 type ChainIdGoban s = A.STUArray s Vertex ChainId
 
+type ChainNeighbours = M.IntMap VertexSet
 
 
 
-showChainIdGoban :: ChainIdGoban s -> String
+
+showChainIdGoban :: ChainIdGoban s -> ST s String
 showChainIdGoban cg = do
   (_, (n, _)) <- A.getBounds cg
   chainIds <- A.getElems cg
-  return $ (++) "\n\n" $ unlines $ reverse $ ((xLegend n) : (show' n (1 :: Int) chainIds))
+  return $ unlines $ reverse $ ([(xLegend n)] ++ (show' n (1 :: Int) chainIds))
     where
+      show' :: Int -> Int -> [ChainId] -> [String]
       show' n _ [] = [xLegend n]
       show' n ln ls' = (ln' ++ concatMap ((" " ++) . show) left ++ ln')
                      : (show' n (ln + 1) right)
@@ -74,65 +86,161 @@ showChainIdGoban cg = do
             (left, right) = splitAt n ls'
             ln' = printf "  %2d " ln
 
+      xLegend :: Int -> String
       xLegend n = "     " ++ concatMap (printf "%2d") [1 .. n]
 
 
 
-newGoban :: Boardsize -> ST s (ChainIdGoban s)
-newGoban n =
-    A.newArray ((1, 1), (n, n)) (0 :: ChainId)
+newChainGoban :: Boardsize -> ST s (ChainIdGoban s)
+newChainGoban n =
+    A.newArray ((1, 1), (n, n)) noChainId
 
 
-vertexChain :: ChainIdGoban s -> ChainMap -> Vertex -> ST s (Maybe Chain)
+
+addStone :: ChainIdGoban s -> ChainMap -> Stone -> ST s (ChainMap)
+addStone cg cm s@(Stone p color) = do
+  (fvs, ours, cns) <- adjacentStuff cg cm s
+
+  (cm', i) <- return $ case ours of
+                         [] ->
+                             mapAddChain cm s fvs cns
+                         fcs ->
+                             (mapFoldChains cm' (i : fcs), i)
+                             where
+                               (cm', i) = mapAddChain cm s fvs cns
+  A.writeArray cg p i
+
+  case cns of
+    _ -> return ()
+
+  return cm'
+    
+
+adjacentStuff :: ChainIdGoban s -> ChainMap -> Stone
+              -> ST s (VertexSet, [ChainId], ChainNeighbours)
+adjacentStuff cg cm (Stone initP color) = do
+    as <- mapM m $ adjacentVertices initP
+    (ls, acs) <- return $ partition ((== noChainId) . fst) as
+    (ours, others) <- return $ partition ((color ==) . chainColor . (idChain cm) . fst) acs
+
+    return $ (fvs ls, map fst ours, cns others)
+    where
+      fvs ls = S.fromList $ map snd ls
+
+      cns others = M.fromListWith S.union
+             $ map (\(k, v) -> (k, S.singleton v)) others
+
+      m p = do
+        v <- A.readArray cg p
+        return (v, p)
+
+
+deleteChain :: ChainIdGoban s -> ChainMap -> ChainId -> ST s (ChainMap)
+deleteChain cg cm i = do
+  mapM_ resetVertex vs
+  return $ mapDeleteChain cm i
+  where
+    resetVertex p = A.writeArray cg p noChainId
+    vs = S.elems $ chainVertices $ idChain cm i
+
+
+
+vertexChain :: ChainIdGoban s -> ChainMap -> Vertex -> ST s Chain
 vertexChain cg cm p = do
-    i <- A.readArray cg p
-    return $ M.lookup i cm
-      
-
-
-
--- addStone :: STGoban s -> Stone -> ST s ()
--- addStone g (Stone vertex color) =
---     writeGoban g vertex (Colored color)
-
--- deleteStones :: STGoban s -> [Stone] -> ST s ()
--- deleteStones g stones =
---     mapM_ (\p -> writeGoban g p Empty) $ map stoneVertex stones
+  i <- A.readArray cg p
+  return $ idChain cm i
 
 
 
 
--- do
---   s <- readGoban g vertex
---   return $ case s of
---              Colored color -> Just $ Stone vertex color
---              Empty -> Nothing
---              Border -> Nothing
-
--- intVertexToStone :: STGoban s -> Int -> ST s (Maybe Stone)
--- intVertexToStone g@(STGoban n _v) i = do
---   s <- intReadGoban g i
---   return $ case s of
---              Colored color -> Just $ Stone (intToVertex n i) color
---              Empty -> Nothing
---              Border -> Nothing
 
 
--- writeGoban :: STGoban s -> Vertex -> VertexState -> ST s ()
--- writeGoban (STGoban n v) vertex state =
---     VM.write v (vertexToInt n vertex) (stateToWord state)
-
--- readGoban :: STGoban s -> Vertex -> ST s VertexState
--- readGoban (STGoban n v) vertex =
---     VM.read v (vertexToInt n vertex) >>= (return . wordToState)
-
--- intReadGoban :: STGoban s -> Int -> ST s VertexState
--- intReadGoban (STGoban _n v) i =
---     VM.read v i >>= (return . wordToState)
 
 
--- gobanSize :: STGoban s -> ST s Boardsize
--- gobanSize (STGoban n _) = return n
+idChain :: ChainMap -> ChainId -> Chain
+idChain cm i =
+    fromMaybe (error "idChain Nothing") $ M.lookup i cm
+
+nextChainId :: ChainMap -> ChainId
+nextChainId cm = head ([1 ..] \\ M.keys cm)
+
+
+
+mapAddChain :: ChainMap -> Stone -> VertexSet -> ChainNeighbours -> (ChainMap, ChainId)
+mapAddChain cm s@(Stone p color) ls ecs =
+    (cm', i)
+    where
+      cm' = M.insert i c cm
+      c = Chain { chainId = i
+                , chainColor = color
+                , chainLiberties = ls
+                , chainVertices = S.singleton p
+                , chainNeighbours = ecs
+                }
+      i = nextChainId cm
+
+-- mapAddToChain :: ChainMap -> ChainId -> Stone -> [Vertex] -> [(ChainId, Vertex)] -> ChainMap
+-- mapAddToChain cm i s@(Stone p color) fs ecs =
+--     M.adjust updateChain i cm
+--     where
+--       updateChain :: Chain -> Chain
+--       updateChain c =
+--           c { chainLiberties = chainLiberties c `S.union` S.fromList fs
+--             , chainVertices = S.insert p $ chainVertices c
+--             , chainNeighbours = error "mapAddToChain FIXME: TODO"
+--             }
+
+mapFoldChains :: ChainMap -> [ChainId] -> ChainMap
+mapFoldChains cm ois@(i : is) =
+    mapFoldChainsNeighbours cm' ois (M.keys cns)
+    where
+      cm' = M.insert i c' cm
+      c' = c { chainLiberties = S.unions $ map chainLiberties cs
+             , chainVertices = S.unions $ map chainVertices cs
+             , chainNeighbours = cns
+             }
+      cns = M.unionsWith S.union $ map chainNeighbours cs
+      cs = c : (map (idChain cm) is)
+      c = idChain cm i
+mapFoldChains _ _ = error "mapFoldChains called with single ChainId"
+
+
+mapFoldChainsNeighbours :: ChainMap -> [ChainId] -> [ChainId] -> ChainMap
+mapFoldChainsNeighbours initCm (i : is) ncs =
+    foldl' updateCM initCm ncs
+    where
+      updateCM :: ChainMap -> ChainId -> ChainMap
+      updateCM cm ni = M.adjust updateNC ni cm
+
+      updateNC :: Chain -> Chain
+      updateNC c =
+          c { chainNeighbours = ncnm' }
+          where
+            ncnm' = M.adjust (l `S.union`) i ncnm
+            l = S.unions $ catMaybes $ map (\j -> M.lookup j ncnm) is
+            ncnm = chainNeighbours c
+mapFoldChainsNeighbours _ _ _ = error "mapFoldChainsNeighbours called with single ChainId"
+
+
+mapDeleteChain :: ChainMap -> ChainId -> ChainMap
+mapDeleteChain initCm i =
+    M.delete i
+         $ foldl' updateCM initCm
+               $ M.assocs
+                     $ chainNeighbours
+                           $ fromMaybe (error "mapDeleteChain Nothing")
+                                 $ M.lookup i initCm
+    where
+      updateCM :: ChainMap -> (ChainId, VertexSet) -> ChainMap
+      updateCM cm (nId, ls) =
+          M.adjust (updateNC ls) nId cm
+      updateNC :: VertexSet -> Chain -> Chain
+      updateNC ls c =
+          c { chainLiberties = chainLiberties c `S.union` ls
+            , chainNeighbours = M.delete i $ chainNeighbours c
+            }
+
+
 
 
 
