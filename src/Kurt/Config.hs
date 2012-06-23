5fe89649b9d9b42115125422e90d9a68d4acbda8
Fabian Linzberger <e@lefant.net>
2012-06-23 21:16:03 +0200

    fix for most recent cmdargs version, part2: correct default values

--- src/Kurt/Config.hs
+++ src/Kurt/Config.hs
@@ -41,30 +41,28 @@ data KurtConfig = KurtConfig { maxPlayouts           :: Int
 
 kurtDefaultConfig :: KurtConfig
 kurtDefaultConfig = KurtConfig {
-               maxPlayouts = def &= typ "INT" &= opt (100000 :: Int)
+               maxPlayouts = (100000 :: Int) &= typ "INT"
                              &= help "Max simulations run during move generation"
-             , maxTime = def &= typ "INT" &= opt (5000 :: Int)
+             , maxTime = (5000 :: Int) &= typ "INT"
                          &= help "Max time used during move generation (ms)"
-             , uctExplorationPercent = def &= typ "INT" &= opt (10 :: Int)
-                                &= help "Exploration constant used in UCT formula (percent)"
-             , raveWeight = def &= typ "INT" &= opt (20 :: Int)
+             , uctExplorationPercent = def &= opt (10 :: Int) &= typ "INT"
+                                       &= help "Exploration constant used in UCT formula (percent)"
+             , raveWeight = (20 :: Int) &= typ "INT"
                                 &= help "Weight used for RAVE in UCT-RAVE formula"
-             , hCaptureWeight = def &= typ "INT" &= opt (25 :: Int)
+             , hCaptureWeight = (25 :: Int) &= typ "INT"
                                 &= help "Weight used for captures in heuristic"
-             , hMinLibertiesWeight = def &= typ "INT" &= opt (7 :: Int)
+             , hMinLibertiesWeight = (7 :: Int) &= typ "INT"
                                 &= help "Weight used for minimum liberties in heuristic"
-             , hLibertiesWeight = def &= typ "INT" &= opt (3 :: Int)
+             , hLibertiesWeight = (3 :: Int) &= typ "INT"
                                 &= help "Weight used for liberties in heuristic"
-             , hChainCountWeight = def &= typ "INT" &= opt (1 :: Int)
+             , hChainCountWeight = (1 :: Int) &= typ "INT"
                                 &= help "Weight used for group count in heuristic"
-             , hCenterWeight = def &= typ "INT" &= opt (5 :: Int)
+             , hCenterWeight = (5 :: Int) &= typ "INT"
                                 &= help "Weight used for center moves in heuristic"
-             , initialKomi = def &= typ "FLOAT" &= opt (7.5 :: Float)
+             , initialKomi = (7.5 :: Float) &= typ "FLOAT"
                              &= help "Initial komi value (usually overridden via GTP)"
-             , initialBoardsize = def &= typ "INT" &= opt (9 :: Int)
+             , initialBoardsize = (9 :: Int) &= typ "INT"
                              &= help "Initial boardsize (usually overridden via GTP)"
-             } &= summary "Kurt - Computer Go program. Implemented as a GTP server to be run from a GTP client like gogui or kgsGTP."
-
-
---             } &= prog "kurt" &= help "Computer Go program. Implemented as a GTP server to be run from a GTP client like gogui or kgsGTP."
-
+             }
+                    &= program "kurt"
+                    &= summary "Computer Go program. Implemented as a GTP server to be run from a GTP client like gogui or kgsGTP."
